bc9fc7d50c07a31a2d651b87c5aa17b6327fd2cd
Gary G <garygan89@yahoo.com>
2020-11-12 11:37:17 +0800

    Add downloadmedia script to download demo media from mediafire direct link instead of Git LFS due to quota limit.

---
+++ demovideo/downloadmedia.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget http://www.mediafire.com/file/529xszmld123d40/Big_Buck_Bunny_1080_10s_30MB.mp4
+wget http://www.mediafire.com/file/of4pxdr2mcxc96g/I_Am_Legend_Trailer.mp4
+wget http://www.mediafire.com/file/wxx8o5hu0ajg4lc/mfssvep-avro-pattern-a.mp4
+wget http://www.mediafire.com/file/tc5wje5kwoxk7ei/SKYFALL-4K.mp4
+wget http://www.mediafire.com/file/jp1heht4j299eyh/trailer_1080p_h264_mp3.avi
